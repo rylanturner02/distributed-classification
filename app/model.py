f5888e123c73c716b1f433fa59168c3ac3b9e738
Rylan Turner <rylanturner02@gmail.com>
2024-06-23 16:08:00 -0500

    Implemented model architecture

---
+++ app/model.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class TreeClassifier(nn.Module):
+    def __init__(self, num_classes=3):
+        super(TreeClassifier, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+        )
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(128, num_classes)
+        )
+        
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
